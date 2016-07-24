6f10e02e25c336eade24b53e4e904ab1f89c70aa
Andrew Cutler <macropin@gmail.com>
2016-07-24 22:09:43 +1000

    Fix nginx and mailman config

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM debian:jessie
 
 RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y nginx mailman postfix supervisor && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y nginx mailman postfix supervisor fcgiwrap multiwatch busybox-syslogd && \
     apt-get -y install vim && \
     rm -rf /var/lib/apt/lists/* && \
-    # Cache dir as template
+    # Cache default dirs as template
+    cp -a /etc/mailman /etc/mailman.cache && \
+    cp -a /var/lib/mailman /var/lib/mailman.cache && \
     cp -a /var/spool/postfix /var/spool/postfix.cache && \
-    cp -a /var/lib/mailman /var/lib/mailman.cache
+    # Configure Nginx
+    sed -i -e 's@worker_processes 4@worker_processes 1@g' /etc/nginx/nginx.conf && \
+    rm -f /etc/nginx/sites-enabled/default
 
 ADD nginx.conf /etc/nginx/conf.d/
 ADD supervisord.conf /etc/supervisor/supervisord.conf
@@ -14,7 +18,7 @@ ADD *.sh /
 
 EXPOSE 25 80
 
-VOLUME ["/etc/mailman/", "/var/lib/mailman", "/var/log/mailman", "/var/spool/postfix"]
+#VOLUME ["/etc/mailman/", "/var/lib/mailman", "/var/log/mailman", "/var/spool/postfix"]
 
 ENTRYPOINT ["/entry.sh"]
 
--- entry.sh
+++ entry.sh
@@ -10,38 +10,23 @@ set -e
 [ -z "$MAILMAN_ADMINMAIL" ] && echo "Error: MAILMAN_ADMINMAIL not set" && exit 128 || true
 [ -z "$MAILMAN_ADMINPASS" ] && echo "Error: MAILMAN_ADMINPASS not set" && exit 128 || true
 
-# function generate_aliases() {
-# LISTNAME=$1
-# cat << EOF >> /etc/aliases
-# ## ${LISTNAME} mailing list
-# ${LISTNAME}:              "|/var/lib/mailman/mail/mailman post ${LISTNAME}"
-# ${LISTNAME}-admin:        "|/var/lib/mailman/mail/mailman admin ${LISTNAME}"
-# ${LISTNAME}-bounces:      "|/var/lib/mailman/mail/mailman bounces ${LISTNAME}"
-# ${LISTNAME}-confirm:      "|/var/lib/mailman/mail/mailman confirm ${LISTNAME}"
-# ${LISTNAME}-join:         "|/var/lib/mailman/mail/mailman join ${LISTNAME}"
-# ${LISTNAME}-leave:        "|/var/lib/mailman/mail/mailman leave ${LISTNAME}"
-# ${LISTNAME}-owner:        "|/var/lib/mailman/mail/mailman owner ${LISTNAME}"
-# ${LISTNAME}-request:      "|/var/lib/mailman/mail/mailman request ${LISTNAME}"
-# ${LISTNAME}-subscribe:    "|/var/lib/mailman/mail/mailman subscribe ${LISTNAME}"
-# ${LISTNAME}-unsubscribe:  "|/var/lib/mailman/mail/mailman unsubscribe ${LISTNAME}"
-# EOF
-# }
+# Copy default mailman etc from cache
+if [ ! "$(ls -A /etc/mailman)" ]; then
+   cp -a /etc/mailman.cache/. /etc/mailman/
+fi
 
-# # Generate all aliases for postfix
-# for LISTDIR in /var/lib/mailman/lists/*; do
-#     LIST=$(basename $LISTDIR)
-#     generate_aliases $LIST
-# done
-
+# Copy default mailman data from cache
+if [ ! "$(ls -A /var/lib/mailman)" ]; then
+   cp -a /var/lib/mailman.cache/. /var/lib/mailman/
+fi
 
 # Copy default spool from cache
 if [ ! "$(ls -A /var/spool/postfix)" ]; then
    cp -a /var/spool/postfix.cache/. /var/spool/postfix/
 fi
 
-if [ ! "$(ls -A /var/lib/mailman)" ]; then
-   cp -a /var/lib/mailman.cache/. /var/lib/mailman/
-fi
+# Fix permissions
+/usr/lib/mailman/bin/check_perms -f
 
 # If master list does not exist, create it
 if [ ! -d /var/lib/mailman/lists/mailman ]; then
