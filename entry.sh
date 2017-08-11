e8def61dea5f681c4836840338098ed15349b742
Richard Brinkman <r.brinkman@gmail.com>
2017-08-11 14:38:17 +0200

    Fix: creation of mail lists

--- entry.sh
+++ entry.sh
@@ -33,6 +33,13 @@ if [ ! -d /var/lib/mailman/lists/mailman ]; then
     /var/lib/mailman/bin/newlist --quiet --urlhost=$MAILMAN_URLHOST --emailhost=$MAILMAN_EMAILHOST mailman $MAILMAN_ADMINMAIL $MAILMAN_ADMINPASS
 fi
 
+# Set mailman admin password
+mmsitepass $MAILMAN_ADMINPASS
+
+# Add virtualhost
+linetoadd="add_virtualhost('$MAILMAN_URLHOST', '$MAILMAN_EMAILHOST')"
+grep -q "$linetoadd" /etc/mailman/mm_cfg.py || echo "$linetoadd" >> /etc/mailman/mm_cfg.py
+
 # Fix Postfix settings
 postconf -e mydestination=${MAILMAN_EMAILHOST}
 postconf -e myhostname=${HOSTNAME}
